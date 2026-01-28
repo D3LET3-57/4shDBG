a0557a2e5a427884c6585748a9dbf487a3f824a9
D3LET3-57 <lalithsaichakilam3@gmail.com>
2026-01-28 09:43:22 +0530

    initial setup

---
+++ include/debugger.h
@@ -0,0 +1,19 @@
+#ifndef DEBUGGER_H
+#define DEBUGGER_H
+
+#include <string>
+
+class debugger {
+    private:
+        std:: string m_prog_name;
+        int pid;
+    
+    public:
+        debugger(const std::string &prog_name, int pid)
+            : m_prog_name{prog_name}, pid{pid} {}
+        void run();
+    private: 
+        void continue_execution();
+        void handle_command(const char* line);
+};
+#endif // DEBUGGER_H
